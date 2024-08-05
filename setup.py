4d2daddc80c759531a659f23257c0b962e88f2e8
Anhtt9x <anhtt454598@gmail.com>
2024-08-05 15:11:31 +0700

    structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup , find_packages
+
+setup(
+    name="LLMOps",
+    version="0.0.0",
+    author="Anhtt9x",
+    author_email="anhtt454598@gmail.com",
+    packages=find_packages(where=[".","src"])
+)
